780a904a5177b17a268e3c95ac26268b1fa7d0e6
Justin <dev-justinc@gmail.com>
2025-01-01 10:11:29 -0500

    CLI

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="networkii-cli",
     version="1.0.0",
-    packages=find_packages(),
+    packages=find_packages(where="src"),     # Tells setuptools to look in src/
+    package_dir={"": "src"},                 # "Root" of packages is src/
     entry_points={
-        'console_scripts': [
-            'networkii=src.cli:main',
+        "console_scripts": [
+            "networkii = cli:main",         # We'll import cli.py as top-level
         ],
     },
-) 
+)
